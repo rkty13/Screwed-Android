ef9915f702dffa50f8f9fb10f4e59dbefe6142a9
Robert <rkty13@gmail.com>
2015-08-01 19:50:28 -0400

    added stuff

--- app/src/main/java/com/ohshootimscrewed/screwed/MainActivity.java
+++ app/src/main/java/com/ohshootimscrewed/screwed/MainActivity.java
@@ -1,7 +1,22 @@
 package com.ohshootimscrewed.screwed;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
 /**
  * Created by robertkim on 8/1/15.
  */
-public class MainActivity {
+public class MainActivity extends Activity {
+
+    private TextView mSearchInput;
+    private Button mSearchButton;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
 }
