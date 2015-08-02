f42326f0692966d1483e4f73f597437fac88296e
Eric <eric.kong98@gmail.com>
2015-08-02 08:22:53 -0400

    Local branch changes

--- app/src/main/java/com/ohshootimscrewed/screwed/MainActivity.java
+++ app/src/main/java/com/ohshootimscrewed/screwed/MainActivity.java
@@ -1,5 +1,8 @@
 package com.ohshootimscrewed.screwed;
 
+import android.content.Context;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.widget.Button;
@@ -17,5 +20,12 @@ public class MainActivity extends FragmentActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        LocationManager mLocManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        LocationListener mlocListener = new MyLocationListener(this);
+
+        mLocManager.requestLocationUpdates( LocationManager.GPS_PROVIDER, 0, 0, mlocListener);
     }
 }
